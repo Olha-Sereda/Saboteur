45839521e74ade2b7b29aed33567ef91c0290370
Olha-Sereda <olchik.sereda@gmail.com>
2024-01-05 18:18:10 +0100

    Merge remote-tracking branch 'origin/main'

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def hello():
         players_number = request.form.get("players_quantity")
         current_game.start_game(int(players_number))
         #return "Players number: "+players_number+"\n" + option
-        return render_template('board.html', build_board=current_game.board.get_board()[0], message=current_game.board.start,
-                               player=current_game.players[current_game.current_player], coords=range(1,46))
+        return render_template('game.html')
     return render_template('index.html')
 
 
@@ -39,7 +38,7 @@ def game():
 
 @app.route("/cards_in_hands")
 def cards_in_hands():
-    return render_template('board.html', player=current_game.players[current_game.current_player])
+    return render_template('cards_in_hands.html', player=current_game.players[current_game.current_player])
 
 
 if __name__ == "__main__":
---
+++ static/js/saboteur.js
@@ -0,0 +1,64 @@
+     function getBoard() {
+         fetch('/board')
+         .then(response => response.text())
+         .then(data => {
+                 document.getElementById("BoardSection").innerHTML = data;
+             })
+         .catch((error) => {
+             console.error('Error:', error);
+            });
+     }
+
+     function getCardsInHands() {
+         fetch('/cards_in_hands')
+         .then(response => response.text())
+         .then(data => {
+                 document.getElementById("CardsInHandSection").innerHTML = data;
+             })
+         .catch((error) => {
+             console.error('Error:', error);
+            });
+     }
+
+
+    let selectedCardId = null;
+    function selectCard(cardId) {
+        selectedCardId = cardId;
+        console.log(`Selected Card ${cardId}`);
+        document.getElementById("handCardID" + cardId).className="CardHighlight";
+    }
+
+    function selectField(event) {
+        if (selectedCardId !== null) {
+            const fieldId = event.target.id;
+            console.log(`Selected Field ${fieldId}`);
+
+            // Отримайте координати рядка і стовпця з fieldId, наприклад, field11
+            const coordinates = fieldId.slice(9).split('');
+            const row = coordinates[0];
+            const column = coordinates[1];
+
+            document.getElementById(fieldId).className="CardHighlight";
+
+            // Відправте POST-запит з даними
+            const data = { cardId: selectedCardId, row, column };
+            fetch('/your_post_endpoint', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+            // Скидання вибору
+            selectedCardId = null;
+        }
+    }
+
